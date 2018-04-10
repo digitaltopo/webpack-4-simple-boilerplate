602b6b46dca00ab0bd5530ce38554fb33c86ca74
Orestis Herodotou <admin@digitaltopo.net>
2018-04-10 00:05:32 -0700

    Use dot-env webpack plugin

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -10,36 +10,36 @@ const PATHS = require("./paths.config");
 
 // Webpack Base Configuration
 const config = {
-  entry: path.join(PATHS.src, "index.js"),
-  progress: true,
-  stats: {
-    colors: false
-    //hash: true,
-    //timings: true,
-    //assets: true,
-    //chunks: true,
-    //chunkModules: true,
-    //modules: true,
-    //children: true,
-  },
-  output: {
-    path: PATHS.build,
-    pathinfo: true,
-    publicPath: "/"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        },
-        options: {
-          cacheDirectory: true
-        }
-      }
-    ]
-  },
-  plugins: []
+    entry: path.join(PATHS.src, "index.js"),
+    progress: true,
+    stats: {
+        colors: false
+        //hash: true,
+        //timings: true,
+        //assets: true,
+        //chunks: true,
+        //chunkModules: true,
+        //modules: true,
+        //children: true,
+    },
+    output: {
+        path: PATHS.build,
+        pathinfo: true,
+        publicPath: "/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+                options: {
+                    cacheDirectory: true
+                }
+            }
+        ]
+    },
+    plugins: [new Dotenv()]
 };
