6bb805bf4817ff2d3455ce4511555934edf67d01
Orestis Herodotou <admin@digitaltopo.net>
2018-08-02 15:03:45 -0700

    watch HTML file

--- config/devServer.config.js
+++ config/devServer.config.js
@@ -1,6 +1,7 @@
-const PATHS = require("./paths.config");
+const PATHS = require('./paths.config');
 
 module.exports = {
+    watchContentBase: true,
     contentBase: PATHS.build,
     progress: true,
     hot: true
