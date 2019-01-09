aa1922c6f60102e80213edd59457fe6b8de33193
Orestis Herodotou <admin@digitaltopo.net>
2019-01-09 15:17:47 -0800

    Use SFC for App.js

--- src/core/App.js
+++ src/core/App.js
@@ -1,24 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {hot} from 'react-hot-loader/root';
 
 import Test from '../components/test';
-/* 
+
 const App = () => (
     <div>
         <h1>Hello React World!</h1>
         <Test />
     </div>
 );
- */
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello React World!</h1>
-                <Test />
-            </div>
-        );
-    }
-}
 
 export default hot(App);
