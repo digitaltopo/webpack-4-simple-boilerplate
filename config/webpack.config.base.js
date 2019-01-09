b488f6e09f74e7997d09382652dcc326caa39364
Orestis Herodotou <admin@digitaltopo.net>
2019-01-09 13:56:41 -0800

    Update linting

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,29 +1,29 @@
 // Imports
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 // Plugins
-const Dotenv = require("dotenv-webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // Check for development mode
-const IS_DEV = process.env.NODE_ENV === "development";
+const IS_DEV = process.env.NODE_ENV === 'development';
 
 // Configurations
-const PATHS = require("./paths.config");
-const stats = require("./stats.config");
+const PATHS = require('./paths.config');
+const stats = require('./stats.config');
 
 // Webpack Base Configuration
 const config = {
-    entry: path.join(PATHS.src, "index.js"),
+    entry: path.join(PATHS.src, 'index.js'),
     stats: stats,
     output: {
         path: PATHS.build,
         pathinfo: true,
-        publicPath: "/"
+        publicPath: '/'
     },
     resolve: {
-        extensions: ["*", ".js", ".jsx"]
+        extensions: ['*', '.js', '.jsx']
     },
     module: {
         rules: [
@@ -32,7 +32,7 @@ const config = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader",
+                    loader: 'babel-loader',
                     options: {
                         cacheDirectory: true
                     }
@@ -41,11 +41,7 @@ const config = {
             // CSS (SCSS) Styles
             {
                 test: /\.(scss|sass|css)$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
+                use: ['style-loader', 'css-loader', 'sass-loader']
             },
             // Web Workers
             {
@@ -63,14 +59,14 @@ const config = {
     },
     plugins: [
         new Dotenv({
-            path: path.join(PATHS.root, ".env"), // load this now instead of the ones in '.env'
+            path: path.join(PATHS.root, '.env'), // load this now instead of the ones in '.env'
             safe: false, // Load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
             systemvars: true, // Load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
             silent: false // Hide errors
         }),
         new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: path.join(PATHS.src, "index.html")
+            filename: 'index.html',
+            template: path.join(PATHS.src, 'index.html')
         })
     ]
 };
