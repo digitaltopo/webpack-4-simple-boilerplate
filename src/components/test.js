a033d129455dc74dbddfe32c7d53bcdc35ec0883
Orestis Herodotou <admin@digitaltopo.net>
2019-01-09 15:14:47 -0800

    Add test component with useState react hook

---
+++ src/components/test.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+const Test = () => {
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <h2>Test useState hook Functional Component</h2>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
+    );
+};
+
+export default Test;
--- src/core/App.js
+++ src/core/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {hot} from 'react-hot-loader/root';
 
+import Test from '../components/test';
+/* 
 const App = () => (
     <div>
         <h1>Hello React World!</h1>
+        <Test />
     </div>
 );
+ */
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello React World!</h1>
+                <Test />
+            </div>
+        );
+    }
+}
 
 export default hot(App);
