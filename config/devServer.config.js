63c496d3c599b74bf82f94ea052a6269fd133c4c
Orestis Herodotou <admin@digitaltopo.net>
2020-03-29 16:09:33 -0700

    Create react app version with react-refresh

--- config/devServer.config.js
+++
@@ -1,9 +0,0 @@
-const PATHS = require('./paths.config');
-
-module.exports = {
-    watchContentBase: true,
-    contentBase: PATHS.build,
-    progress: false,
-    hot: true,
-    historyApiFallback: true
-};
--- config/paths.config.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-module.exports = {
-    root: path.resolve(__dirname, ".."),
-    config: path.resolve(__dirname),
-    src: path.resolve(__dirname, "../src"),
-    build: path.resolve(__dirname, "../dist")
-};
